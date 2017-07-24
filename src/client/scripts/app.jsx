7f127aadc16f9960bd18d2376014a9c895e959cc
Lars Lütjens <lars.luetjens@tum.de>
2017-07-24 21:08:33 +0200

    Better indentation

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -80,12 +80,10 @@ export class App extends React.Component {
 
     //all contracts
     this.contract = parity.bonds.makeContract('0xca16D554f2974F32C16212c6C39e678dA958b50e', abi.getStorageABI()); // v0.6
-
     this.immigration = parity.bonds.makeContract('0x42Da049B7E5c7AAcF5506cE7198b1a7B23070C93', abi.getImmigrationABI()); //v0.8
     this.citizen = parity.bonds.makeContract('0x90f8092B9f6E596D8D2937c971D64B93f866dD80', abi.getCitizenABI()); // v0.4
     this.nation = parity.bonds.makeContract('0x75e0292B68FaADc14B4Ac64E7ED6D6A1b2706654', abi.getNationABI()); // v0.5
     this.embassy = parity.bonds.makeContract('0x6b96d593126ACa0c135791542d0C10593116bB31', abi.getEmbassyABI()); // v0.4
-
     this.nameresolver = parity.bonds.makeContract('0x53708Ea1EF858086Afcb2E063E5CA7CDF7EC9d76', abi.getNameresolverABI()); // v0.4
 
     this.countryCode = abi.getCountryCode();
@@ -482,10 +480,10 @@ export class App extends React.Component {
             <h1>Scan QR-Code or enter Wallet-ID</h1>
             <Divider/>
             <QrReader
-         style={previewStyle}
-         onError={this.handleError.bind(this)}
-         onScan={this.handleScan.bind(this)}
-         />
+              style={previewStyle}
+              onError={this.handleError.bind(this)}
+              onScan={this.handleScan.bind(this)}
+            />
          <Divider/>
             <TextField hintText="Wallet-ID" underlineShow={false} fullWidth={true} onChange={e => this.checkIfAddress(e)}/>
             <Divider/>
@@ -500,99 +498,91 @@ export class App extends React.Component {
     if (this.state.userType == 'embassy' && !this.state.enteredValidation) {
       document.body.style.backgroundColor = "#BD804B";
       return (
-
         <div>
           <div onClick={this.resetApp.bind(this)}>
-          <Logo />
+            <Logo />
           </div>
 
           <Paper style={paperStyle} zDepth={5}>
-          <div>
-          <Tabs
-                 value={this.state.value}
-                 onChange={this.handleChange}
-               >
-                 <Tab label="Offerings" value="a">
-
-                 <div>
-                 <Card>
-                 <DialogExampleModal2/>
-                 <LinearProgress mode="indeterminate" />
-                   <CardHeader
-                     title="Visum A1"
-                     subtitle="Transit"
-                     actAsExpander={true}
-                     showExpandableButton={true}
-                   />
-                   <CardActions>
-                     <FlatButton label="Delete" />
-                   </CardActions>
-                   <CardText expandable={true}>
-                     Transit (C) visas are nonimmigrant visas for persons traveling in immediate and continuous transit
-                     through the United States en route to another country, with few exceptions. Immediate and continuous
-                     transit is defined as a reasonably expeditious departure of the traveler in the normal course of travel
-                     as the elements permit and assumes a prearranged itinerary without any unreasonable layover privileges.
-                   </CardText>
-                 </Card>
-                 <Card>
-                   <CardHeader
-                     title="Visum B1"
-                     subtitle="Social"
-                     actAsExpander={true}
-                     showExpandableButton={true}
-                   />
-                   <CardActions>
-                     <FlatButton label="Modify" />
-                     <FlatButton label="Delete" />
-                   </CardActions>
-                   <CardText expandable={true}>
-                   Social Cultural Visa is issued to travelers who intend to visit Indonesia for: Lecture, short Internship program,
-                   short Courses, Arts, Meetings, Volunteer Program, Sport Activities, visiting family and other related Social
-                   activities.
-                   Social Cultural Visa (Single or Multiple Entry Visa) will allow you a maximum stay of 60 (sixty) days for each visit. This type of visa can be extended at the Indonesian Immigration Office for 4 (four) times, with each extension for maximum 30 (thirty) days.
-                   </CardText>
-                 </Card>
-                 <Card>
-                   <CardHeader
-                     title="Visum C1"
-                     subtitle="Business Visitor"
-                     actAsExpander={true}
-                     showExpandableButton={true}
-                   />
-                   <CardActions>
-                     <FlatButton label="Modify" />
-                     <FlatButton label="Delete" />
-                   </CardActions>
-                   <CardText expandable={true}>
-                   If the purpose of the planned travel is business related, for example, to consult with business associates, attend a scienti c, educational, professional or business conference, settle an estate, or negotiate a contract, then a business visitor visa (B-1) would be the appropriate type of visa for the travel.            </CardText>
-                 </Card>
-
-
-                 </div>
-
-
-                 </Tab>
-                 <Tab label="Validate Pass" value="b">
-                   <div>
+            <div>
+              <Tabs value={this.state.value} onChange={this.handleChange}>
+                <Tab label="Offerings" value="a">
+                  <div>
+                    <Card>
+                      <DialogExampleModal2/>
+                      <CardHeader
+                        title="Visum A1"
+                        subtitle="Transit"
+                        actAsExpander={true}
+                        showExpandableButton={true}
+                      />
+                      <CardActions>
+                        <FlatButton label="Delete" />
+                      </CardActions>
+                      <CardText expandable={true}>
+                        Transit (C) visas are nonimmigrant visas for persons traveling in immediate and continuous transit
+                        through the United States en route to another country, with few exceptions. Immediate and continuous
+                        transit is defined as a reasonably expeditious departure of the traveler in the normal course of travel
+                        as the elements permit and assumes a prearranged itinerary without any unreasonable layover privileges.
+                      </CardText>
+                    </Card>
+                    <Card>
+                      <CardHeader
+                        title="Visum B1"
+                        subtitle="Social"
+                        actAsExpander={true}
+                        showExpandableButton={true}
+                      />
+                      <CardActions>
+                        <FlatButton label="Modify" />
+                        <FlatButton label="Delete" />
+                      </CardActions>
+                      <CardText expandable={true}>
+                        Social Cultural Visa is issued to travelers who intend to visit Indonesia for: Lecture, short Internship program,
+                        short Courses, Arts, Meetings, Volunteer Program, Sport Activities, visiting family and other related Social
+                        activities.
+                        Social Cultural Visa (Single or Multiple Entry Visa) will allow you a maximum stay of 60 (sixty) days for each visit. This type of visa can be extended at the Indonesian Immigration Office for 4 (four) times, with each extension for maximum 30 (thirty) days.
+                      </CardText>
+                    </Card>
+                    <Card>
+                      <CardHeader
+                        title="Visum C1"
+                        subtitle="Business Visitor"
+                        actAsExpander={true}
+                        showExpandableButton={true}
+                      />
+                      <CardActions>
+                        <FlatButton label="Modify" />
+                        <FlatButton label="Delete" />
+                      </CardActions>
+                      <CardText expandable={true}>
+                      If the purpose of the planned travel is business related, for example, to consult with business associates, attend a scienti c, educational, professional or business conference, settle an estate, or negotiate a contract, then a business visitor visa (B-1) would be the appropriate type of visa for the travel.            </CardText>
+                    </Card>
+                  </div>
+                </Tab>
+                <Tab label="Validate Pass" value="b">
+                  <div>
                     <h1>Scan QR-Code or enter Wallet-ID</h1>
-                    <Divider/>
+                    <Divider />
                     <QrReader
-                 style={previewStyle}
-                 onError={this.handleError.bind(this)}
-                 onScan={this.handleScan.bind(this)}
-                 />
-                 <Divider/>
+                      style={previewStyle}
+                      onError={this.handleError.bind(this)}
+                      onScan={this.handleScan.bind(this)}
+                      />
+                    <Divider />
                     <TextField hintText="Wallet-ID" underlineShow={false} fullWidth={true} onChange={e => this.checkIfAddress(e)}/>
-                    <Divider/>
-                    <RaisedButton style={{
-                      marginTop: 15
-                    }} label="Open" fullWidth={true} disabled={!this.state.immigrationAddressIsAddress} onTouchTap={this.checkWalletPass.bind(this)} />
-                   </div>
-                 </Tab>
-               </Tabs>
-
+                    <Divider />
+                    <RaisedButton
+                      style={{marginTop: 15}}
+                      label="Open"
+                      fullWidth={true}
+                      disabled={!this.state.immigrationAddressIsAddress}
+                      onTouchTap={this.checkWalletPass.bind(this)}
+                      />
+                  </div>
+                </Tab>
+              </Tabs>
             </div>
-
           </Paper>
         </div>
       );
